056fea2c5e294fecb90d5b4b1f8e99cd5fe141a8
wducharme <wesleyducharme1997@gmail.com>
2022-10-08 16:48:14 -0400

    Using proper SHA512

--- ports/nerdfont-codepoints/portfile.cmake
+++ ports/nerdfont-codepoints/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO WesleyDucharme/nerdfont-codepoints
   REF 43c9dde6e5b0b492459565d91c2b2bbcd0dbb0b1
-  SHA512 0
+  SHA512 19450ca856dc54d72fba75f0dd2889f01dabc98400236a660df0019dad12a94c5ba2b8e2f584e68aca1ea63ebfd91cff29d47807f9178fd4177cd399d06118ff
   HEAD_REF main
 )
 
