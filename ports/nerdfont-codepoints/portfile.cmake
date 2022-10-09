0351ac6ad89b74a4a1f9afce5bdf3019fce9594e
wducharme <wesleyducharme1997@gmail.com>
2022-10-09 18:31:19 -0400

    Wiping sha512

--- ports/nerdfont-codepoints/portfile.cmake
+++ ports/nerdfont-codepoints/portfile.cmake
@@ -8,7 +8,7 @@ vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO WesleyDucharme/nerdfont-codepoints
   REF 447c118ca9c44df8e8d3a4177921b86f65443f83
-  SHA512 d83a4ca1f212685be6e90f13a7a6a24831f3c8dedb86ff1bb5b4b7d72230e5924f1d57f3fd778ae1f1a647d8506595d7845bb0d67d045e3eead1822f8caf9570
+  SHA512 0
   HEAD_REF main
 )
 
